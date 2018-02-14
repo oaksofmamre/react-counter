30783e20a2741dab47f21975fa86edf86a7e6f0d
DC <heyyodc-git@yahoo.com>
2018-02-13 17:32:50 -0800

    baseline counter functionality

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CounterContainer />
+      </div>
+    );
+  }
+}
+
+class CounterContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+    this.onIncrement = this.onIncrement.bind(this);
+    this.onDecrement = this.onDecrement.bind(this);
+  }
+  onIncrement() {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  }
+  onDecrement() {
+    this.setState({
+      counter: this.state.counter - 1
+    });
+  }
+  render() {
+    return (
+      <CounterPresenter
+        counter={this.state.counter}
+        onIncrement={this.onIncrement}
+        onDecrement={this.onDecrement}
+      />
+    );
+  }
+}
+
+function CounterPresenter(props) {
+  const { counter, onIncrement, onDecrement } = props;
+  return (
+    <div>
+      <p>{counter}</p>
+      <button type="button" onClick={onIncrement}>
+        Increment
+      </button>
+      <button type="button" onClick={onDecrement}>
+        Decrement
+      </button>
+    </div>
+  );
+}
+
+export default App;
